3c4307caad7e50ba42e9d7166c04de5d23373900
seoyeonpp <seoyeonpub@gmail.com>
2025-09-29 14:57:57 +0900

    talk 작업중..

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,22 +1,29 @@
 export default function Home() {
-  // HTML을 먼저 반환하고, DOM에 추가된 후에 이벤트 리스너를 등록
+  // // HTML을 먼저 반환하고, DOM에 추가된 후에 이벤트 리스너를 등록
   setTimeout(() => {
     const landingText = document.getElementById('landing-text')
     if (landingText) {
-      landingText.addEventListener('click', () => {
-        landingText.innerHTML = 'GO TO TALK'
-        setTimeout(() => {
-          window.location.href = '/talk'
-        }, 500)
-      })
+      landingText.style.opacity = '0'
+      const talkButton = document.getElementById('talk-button')
+      if (talkButton) {
+        talkButton.style.opacity = '1'
+        talkButton.style.height = 'auto'
+        talkButton.style.visibility = 'visible'
+      }
     }
-  }, 0)
+  }, 5000)
 
   return `
-    <main id="landing-text">
-      <h1>
+    <main>
+      <h1 id="landing-text">
        MADELEINE MEMORY RIVERSIDE TALK 2025
       </h1>
+      <div id="talk-button" style="opacity: 0; height: 0; visibility: hidden; transition: opacity 0.5s ease-in-out;">
+         <a href="/talk" >
+            GO TO TALK
+            <i></i>
+          </a>
+       </div>
       <aside class="water"></aside>
     </main>
     <svg color-interpolation-filters="sRGB">
@@ -32,6 +39,7 @@ export default function Home() {
       </filter>
     </svg>
     
+    
     <style>
       @import url("https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap");
       :root {
@@ -40,7 +48,7 @@ export default function Home() {
         --clr-text: #fff;
       }
       body {
-        height: 100svh;
+        height: 100%;
         display: grid;
         place-items: center;
         font-family: "Noto Serif", serif;
@@ -113,6 +121,116 @@ export default function Home() {
           background-position: 0% 50%;
         }
       }
+      
+      a {
+        position:relative;
+        display: inline-block;
+        text-transform: uppercase;
+        text-decoration: none;
+        letter-spacing: 5px;
+        color: #FAFAFA;
+        border: 3px solid #FAFAFA;
+        padding: 25px;
+        
+      /*  centering */
+        text-align: center;
+        background: rgba(0,0,0,0.5);
+        @include fade-transition(background);
+
+        &:hover {
+          background: rgba(8,97,76,0.6);
+        }
+      }
+
+
+    i {
+    /*   positioning */
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        left: 0;
+      
+    /*   gradient   */
+        background: -moz-linear-gradient(left,  rgba(255,255,255,0) 0%, rgba(255,255,255,0.03) 1%, rgba(255,255,255,0.6) 30%, rgba(255,255,255,0.85) 50%, rgba(255,255,255,0.85) 70%, rgba(255,255,255,0.85) 71%, rgba(255,255,255,0) 100%); /* FF3.6+ */
+        background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(255,255,255,0)), color-stop(1%,rgba(255,255,255,0.03)), color-stop(30%,rgba(255,255,255,0.85)), color-stop(50%,rgba(255,255,255,0.85)), color-stop(70%,rgba(255,255,255,0.85)), color-stop(71%,rgba(255,255,255,0.85)), color-stop(100%,rgba(255,255,255,0))); /* Chrome,Safari4+ */
+        background: -webkit-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%); /* Chrome10+,Safari5.1+ */
+        background: -o-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%); /* Opera 11.10+ */
+        background: -ms-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%); /* IE10+ */
+        background: linear-gradient(to right,  rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%); /* W3C */
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 */
+        
+    /*  forming the shine element
+        play around with the width, skew and gradient to get different effects
+      */
+        width: 15%;
+        height: 100%;
+        transform: skew(-10deg,0deg);
+        -webkit-transform: skew(-10deg,0deg);
+        -moz-transform: skew(-10deg,0deg);
+        -ms-transform: skew(-10deg,0deg);
+        -o-transform: skew(-10deg,0deg);
+      
+    /*  animating it  */
+        animation: move 2s;
+        animation-iteration-count: infinite;
+        animation-delay: 1s;
+        -webkit-animation: move 2s;
+        -webkit-animation-iteration-count: infinite;
+        -webkit-animation-delay: 1s;
+        -moz-transform: skew(-10deg,0deg);
+        -moz-animation: move 2s;
+        -moz-animation-iteration-count: infinite;
+        -moz-animation-delay: 1s;
+        -ms-transform: skew(-10deg,0deg);
+        -ms-animation: move 2s;
+        -ms-animation-iteration-count: infinite;
+        -ms-animation-delay: 1s;
+        -o-transform: skew(-10deg,0deg);
+        -o-animation: move 2s;
+        -o-animation-iteration-count: infinite;
+        -o-animation-delay: 1s;
+      }
+
+
+    @keyframes move {
+      0%  { left: 0; opacity: 0; }
+      5% {opacity: 0.0}
+      48% {opacity: 0.2}
+      80% {opacity: 0.0}
+      100% { left: 82%}
+    }
+
+    @-webkit-keyframes move {
+      0%  { left: 0; opacity: 0; }
+      5% {opacity: 0.0}
+      48% {opacity: 0.2}
+      80% {opacity: 0.0}
+      100% { left: 82%}
+    }
+
+    @-moz-keyframes move {
+      0%  { left: 0; opacity: 0; }
+      5% {opacity: 0.0}
+      48% {opacity: 0.2}
+      80% {opacity: 0.0}
+      100% { left: 88%}
+    }
+
+    @-ms-keyframes move {
+      0%  { left: 0; opacity: 0; }
+      5% {opacity: 0.0}
+      48% {opacity: 0.2}
+      80% {opacity: 0.0}
+      100% { left: 82%}
+    }
+
+    @-o-keyframes move {
+      0%  { left: 0; opacity: 0; }
+      5% {opacity: 0.0}
+      48% {opacity: 0.2}
+      80% {opacity: 0.0}
+      100% { left: 82%}
+    }
     </style>
   `
 }
--- src/pages/talk.ts
+++ src/pages/talk.ts
@@ -1,8 +1,115 @@
 export default function Talk() {
+  setTimeout(() => {
+    const card = document.querySelector('.card')
+    function clickRotate() {
+      card!.classList.toggle('rotated')
+    }
+    if (card) {
+      card.addEventListener('click', clickRotate)
+    }
+  }, 0)
   return `
-    <div>
-      <h1>Talk</h1>
-      <p>Welcome to talk page</p>
+    <div class="cards-wrapper">
+      <div class="card-container">
+        <div class="card">
+          <div class="card-contents card-front">
+            <div class="card-depth">
+              <h2>Click card</h2>
+              <hr>
+              <p>For 3D rotation</p>
+            </div>
+          </div>
+          <div class="card-contents card-back">
+            <div class="card-depth">
+              <h2>Click card again</h2>
+              <hr>
+              <p>To turn back</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <style>
+    body{
+      background: linear-gradient(-45deg, #FFE8DB, #739EC9, #000000, #5682B1);
+      background-size: 400% 400%;
+      animation: gradient 10s ease infinite;
+    }
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
+    // CARD
+    .cards-wrapper {
+      margin-top: 50px;
+    }
+    .card-container {
+      perspective: 1200px;
+    }
+    .card {
+      margin: 0 auto;
+      height: 500px;
+      width: 100%;
+      max-width: 80%;
+      position: relative;
+      border-radius: 25px;
+      transition: all 1s ease;
+      transform-style: preserve-3d;
+      box-shadow: 1px 3px 3px rgba(0,0,0,0.2)
+    }
+
+    .rotated {
+      transform: rotateY(-180deg);
+    }
+    .card-contents {
+      width: 100%;
+      height: 100%;
+      border-radius: 25px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      text-align: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      backface-visibility: hidden;
+      h2, p {
+        text-shadow: 1px 2px 2px rgba(0,0,0,0.2);
+      }
+      h2 {
+        font-size: 3em;
+        margin: 0 auto;
+      }
+      hr {
+        width: 50%;
+        margin: 20px auto;
+      }
+      p {
+        margin: 0 auto;
+      }
+    }
+    .card-depth {
+      transform: translateZ(100px) scale(0.98);
+      perspective: inherit;
+
+    }
+    .card-front {
+      background: linear-gradient(to top left, #3a6186, #89253e);           
+      transform-style: preserve-3d;
+    }
+    .card-back {
+      transform: rotateY(180deg);
+      background: linear-gradient(to top left, #ffa17f, #00223e);
+      transform-style: preserve-3d;
+    }
+    </style>
   `
 }
